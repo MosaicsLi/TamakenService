d656b42e05e3581d6cb35b1b7c43ce94974c0cad
DESKTOP-HL58G6I\tamak <tamaken571@gmail.com>
2023-12-29 15:32:20 +0800

    補上NLOG

---
+++ Log/NlogService.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamakenService.Log
+{
+    public class NlogService
+    {
+        private ILogger _logger;
+        public NlogService(string logFilePath)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var fileTarget = new NLog.Targets.FileTarget("fileTarget")
+            {
+                FileName = logFilePath,
+                Layout = "${longdate} ${level} ${message} ${exception:format=tostring}"
+            };
+
+            config.AddRuleForAllLevels(fileTarget);
+
+            LogManager.Configuration = config;
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogError(string message, System.Exception ex = null)
+        {
+            _logger.Error(ex, message);
+        }
+    }
+}
